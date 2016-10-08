efc87b17f2d7f4063d012df00c6f4f4b7fbbec5f
Ilya Nee <neeilya@mail.ru>
2016-10-08 22:17:31 +0200

    Cleaned up background script

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,7 @@
-import fetcher from './services/jobs-fetcher';
+import jobsFetcher from './services/jobs-fetcher';
 import jobsStorage from './services/jobs-storage';
+import jobsAlarm from './services/jobs-alarm';
 import badge from './services/badge';
-import storage from './services/storage';
 import config from './services/config';
 
 let notifications = 0;
@@ -12,43 +12,14 @@ if(jobsStorage.getUnreadJobs().length > 0) {
     badge.setCounter(jobsStorage.getUnreadJobs().length);
 }
 
-chrome.alarms.create('jobsFetch', { periodInMinutes: config.getInterval() }); // in production minimum 1 minute
+jobsAlarm.create(config.getInterval()); // in production minimum 1 minute
+jobsFetcher.fetchAndNotify();
 
 chrome.alarms.onAlarm.addListener(({ name }) => {
-    if(name !== 'jobsFetch') { return; }
-
-    try {
-        let { results } = fetcher.fetch();
-        let freshJobs = jobsStorage.push(results);
-        let unreadJobs = jobsStorage.getUnreadJobs();
-
-        badge.getText((text) => {
-            if(text === 'err') {
-                storage.store('auth', true);
-                badge.setCounter(unreadJobs.length);
-            }
-        });
-
-        if(freshJobs.length > 0) {
-            badge.setCounter(freshJobs.length);
-
-            chrome.notifications.create(++notifications + '-notification', {
-                type: 'basic',
-                iconUrl: './notification-icon.png',
-                title: 'You got ' + freshJobs.length + ' new jobs!',
-                message: "Don't miss your chance",
-                buttons: [{
-                    title: "Click here to take a look"
-                }]
-            });
-        }
-    } catch(err) {
-        if(err.code === 401) {
-            storage.store('auth', false);
-            badge.refresh();
-            badge.setText('err');
-        }
-
-        console.log(err);
+    if(name !== 'jobsFetch') {
+        return;
     }
+
+    jobsStorage.removeOldJobs();
+    jobsFetcher.fetchAndNotify();
 });
