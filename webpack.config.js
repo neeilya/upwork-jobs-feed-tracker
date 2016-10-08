4d8c87a0ba9e5d6925068367f8e0c69230a5a049
Ilya Nee <neeilya@mail.ru>
2016-10-08 15:54:09 +0200

    Updated webpack config, multuple entries/outputs configured

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,28 @@
 var webpack = require('webpack');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 var CleanWebpackPlugin = require('clean-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    entry: "./src/js/background.js",
+    entry: {
+        background: "./src/js/background.js",
+        popup: "./src/js/popup.js",
+        options: "./src/js/options.js"
+    },
     output: {
-        filename: "public/background.js"
+        filename: "public/[name].js"
     },
     watch: true,
     plugins: [
-        new  CopyWebpackPlugin([
+        new ExtractTextPlugin("public/[name].css"),
+        new CopyWebpackPlugin([
             { from: './manifest.json', to: './public/' },
             { from: './icon.png', to: './public/' },
+            { from: './notification-icon.png', to: './public/' },
+            { from: './src/fonts', to: './public/fonts' },
             { from: './src/views', to: './public' }
         ]),
-        new CleanWebpackPlugin(['public']),
+        new CleanWebpackPlugin(['public'])
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
@@ -32,8 +40,16 @@ module.exports = {
                 }
             },
             {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                loader: 'file-loader?name=fonts/[name].[ext]'
+            },
+            {
                 test: /\.json$/,
                 loader: 'json-loader'
+            },
+            {
+                test: /\.sass$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader?-url!sass-loader')
             }
         ]
     }
