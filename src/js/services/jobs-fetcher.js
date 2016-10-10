a8480509cfcbc10f6ffd5f2a9d59572d0098030d
Ilya Nee <neeilya@mail.ru>
2016-10-10 22:46:51 +0200

    Fix: show correct unread jobs counter on badge

--- src/js/services/jobs-fetcher.js
+++ src/js/services/jobs-fetcher.js
@@ -64,7 +64,7 @@ export default {
             });
 
             if(freshJobs.length > 0) {
-                badge.setCounter(freshJobs.length);
+                badge.setCounter(unreadJobs.length);
 
                 this.removeOldNotifications();
 
