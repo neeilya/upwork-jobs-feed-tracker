e741b50f734e44ced3964b283b1317cb8e7c47e3
Ilya Nee <neeilya@mail.ru>
2017-05-14 15:14:30 +0200

    Setters/getters for config flags

--- src/js/services/config.js
+++ src/js/services/config.js
@@ -6,12 +6,39 @@ export default {
     getInterval() {
         return localStorage.getItem('fetchInterval') || 1; // one minute by default in production (6 seconds in development)
     },
-
     /**
      * Setting period of fetching jobs
-     * @param {Void}
+     * @param interval
      */
     setInterval(interval) {
-        localStorage.setItem('fetchInterval', +interval);
+        localStorage.setItem('fetchInterval', interval);
+    },
+    /**
+     * Getter for playNotificationSound flag
+     * @returns {boolean}
+     */
+    getPlayNotificationSound() {
+        return localStorage.getItem('playNotificationSound');
+    },
+    /**
+     * Setter for playNotificationSound flag
+     * @param value
+     */
+    setPlayNotificationSound(value) {
+        localStorage.setItem('playNotificationSound', +value);
+    },
+    /**
+     * Getter for isFetchingEnabed param
+     * @returns {boolean}
+     */
+    getIsFetchingEnabled() {
+        return localStorage.getItem('isFetchingEnabled');
+    },
+    /**
+     * Getter for isFetchingEnabed param
+     * @returns {boolean}
+     */
+    setIsFetchingEnabled(value) {
+        return localStorage.setItem('isFetchingEnabled', value);
     }
 }
